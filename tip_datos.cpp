f46703de9750152f8ef5dde6d8e47b733a4e60fd
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-10-06 21:00:04 -0600

    tipos de datos basicos clase 2

---
+++ tip_datos.cpp
@@ -0,0 +1,27 @@
+//tipos de datos basicos c++
+
+//libreria principal 
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int numero = 15;
+
+    float flotante = 10.45;
+
+    cout<<numero;
+
+    cout<<flotante;
+
+    double mayor = 16.3456;
+    
+    cout<<mayor;
+
+    char letra = 'a';
+
+    cout<<letra;
+
+    return 0;
+}
