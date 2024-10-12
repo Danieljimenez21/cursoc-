0213b3e67833a5f0e4c1b381ae532809f045a271
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-10-12 12:19:19 -0600

    ejercicio 2 calcular precio sin iva completado

--- clase1/ejercicio2_c6.cpp
+++ clase1/ejercicio2_c6.cpp
@@ -1,5 +1,19 @@
 /*
 escribe un programa que lea la entrada estandar el precio de un 
 producto y mestre en la salidad estandar el precion del producto 
-al aplicarle el IVA
+al aplicarle el IVA 
 */
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float num, iva = .84, total;
+    cout<<"Digite el precio de la compra total ";
+    cin>>num;
+    total = num * iva;
+    cout<<"El total a pagar es: "<<total;
+
+    
+return 0;
+}
