2a0f942d914c52f607aab1f41a2a08b0c1b6dd95
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-10-23 20:17:10 -0600

    ejercico clase 9 terminado de expresiones

---
+++ clase7u2/ejercicio5c8.cpp
@@ -0,0 +1,13 @@
+/* 5. escriba un fragmento de programa que 
+intercambie los valores de dos variabels */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float x, y, z;
+    
+
+    return 0;
+}
