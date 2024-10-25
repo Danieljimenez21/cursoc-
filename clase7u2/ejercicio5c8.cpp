317f0004d6ea8ccd28de8d4343b4b25ddc7e2332
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-10-24 20:11:38 -0600

    clase 8 y ejercicio terminado, intercambio de variables

--- clase7u2/ejercicio5c8.cpp
+++ clase7u2/ejercicio5c8.cpp
@@ -1,13 +1,21 @@
 /* 5. escriba un fragmento de programa que 
-intercambie los valores de dos variabels */
+intercambie los valores de dos variables */
 
 #include<iostream>
 
 using namespace std;
 
 int main(){
-    float x, y, z;
-    
+    float x, y, z = 0;
+    cout<<"Digite el valor de x: "; cin>>x;
+    cout<<"Digite el valor de y: "; cin>>y;
+
+    z = x;
+    x = y;
+    y = z;
+
+    cout<<"El valor de x es: "<<x;
+    cout<<"\nEl valor de y es: "<<y<<endl;
 
     return 0;
 }
