97edf1cfbd05bd7cfd65866763578cbe46da0009
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-10-26 18:49:49 -0600

    ejercicio 8 terminado devolver la hipotenusa de un triangulo

--- clase7u2/ejercicio8.cpp
+++ clase7u2/ejercicio8.cpp
@@ -1,3 +1,24 @@
 /*Escriba un programaq que lea de la entrada estandar 
 los dos catetos de un triangulo rectangulo y escriba en 
-la salida estandar su hipotenusa*/
+la salida estandar su hipotenusa a*/
+
+#include<iostream>
+#include<cmath>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    float a, b, c;
+    cout<<"Digite el cateto a: "; cin>>a;
+    cout<<"Digite el cateto b: "; cin>>b;
+
+    c = sqrt((pow(a,2))+(pow(b,2)));
+
+    cout.precision(4);
+
+    cout<<"la hipotenusa es: "<<c<<endl;
+
+
+    return 0;
+}
