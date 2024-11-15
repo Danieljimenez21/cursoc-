e9916950550310781b21e1e8f77eb63a24813afb
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-11-15 10:54:31 -0600

    clase do while terminada

---
+++ estructureasRepetitivas/ejercicio1.cpp
@@ -0,0 +1,25 @@
+/* La sentencia while:
+    while (Espresion logica){
+    conjunto de instrucciones
+    }
+*/
+
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+    int i;
+    i = 100;
+
+    while (i>=1)
+    {
+        cout<<i<<endl;
+
+        i--;
+    }
+    
+
+    return 0;
+}
